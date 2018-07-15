b4ed6a3543a1251b1a8ce03162788d4e8d209c5e
Lila Feldmann <Lila@Lilas-MacBook-Air.local>
2018-07-15 13:22:55 -0400

    Portfolio Update

---
+++ index.js
@@ -0,0 +1,49 @@
+var portfolio = new Vue({
+  el: "#header",
+  data:{
+    title: "Lila Feldmann"
+    subtitle: "Journey Portfolio"
+  }
+})
+var portfolio = new Vue({
+  
+  el:"#one",
+    data:{
+        title: "Lila Feldmann Journey Portfolio"
+        text: "Welcome to my Journey Portfolio! feel free to look at the projects I've been working on!!"
+        subtitle1: "Yay Vue!"
+    }
+})
+var portfolio = new Vue ({
+    el: "#two",
+    data: {
+        title: "Things I Can do"
+        subtitle1: "A few things that I've learned how to do this summer!"
+        items:[
+            {message: 'Edit text in an html file.'},
+            {message: 'Change template icons'},
+            {message: 'strong immune systems'}
+        ]
+      }
+    })
+  var portfolio = new Vue({
+    el: "#three"
+    data: {
+        message: "A Few Accomplishments"
+        subtitle1: "These are a few projects that I've been working really hard on"
+        items: [
+            {message: "My Portfolio Page"}
+            {message: "Learning how to code basic html"}
+        ]
+    }
+  })
+  var app = new Vue {(
+    el: "#signup-form",
+    data: {
+      name: 'ASDF',
+      email: 'ASDF',
+      subject: 'ASDF',
+      message: 'ASDF'
+    }
+  )},
+   
